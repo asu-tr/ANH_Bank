4bd93a335a7bfc1486c73314753f18234e21f2a6
asu-tr <29000874+asu-tr@users.noreply.github.com>
2022-02-20 23:53:04 +0300

    end of 2022-02-20

--- ANHColorsFonts.cs
+++ ANHColorsFonts.cs
@@ -5,7 +5,8 @@ namespace ANH_Bank
     internal class ANHColorsFonts
     {
         public static Font FontANH_header1 = new Font("Segoe UI Black", 20, FontStyle.Bold);
-        public static Font FontANH_header2 = new Font("Segoe UI Black", 14, FontStyle.Bold);
+        public static Font FontANH_header2 = new Font("Segoe UI Black", 15, FontStyle.Bold);
+        public static Font FontANH_header3 = new Font("Segoe UI Black", 11, FontStyle.Bold);
         public static Font FontANH_default = new Font("Segoe UI Black", 8, FontStyle.Regular);
     }
 }
