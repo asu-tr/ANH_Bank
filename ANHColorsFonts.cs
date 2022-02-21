bc1f1ec75ab9cef7ff8eca4638835a72fdb8db30
asu-tr <29000874+asu-tr@users.noreply.github.com>
2022-02-21 14:28:30 +0300

    changed variable names

--- ANHColorsFonts.cs
+++ ANHColorsFonts.cs
@@ -4,19 +4,19 @@ namespace ANH_Bank
 {
     internal class ANHColorsFonts
     {
-        public static Font FontANH_header1 = new Font("Segoe UI Black", 20, FontStyle.Bold);
-        public static Font FontANH_header2 = new Font("Segoe UI Black", 15, FontStyle.Bold);
-        public static Font FontANH_header3 = new Font("Segoe UI Black", 11, FontStyle.Bold);
+        public static Font Font_header1 = new Font("Segoe UI Black", 20, FontStyle.Bold);
+        public static Font Font_header2 = new Font("Segoe UI Black", 15, FontStyle.Bold);
+        public static Font Font_header3 = new Font("Segoe UI Black", 11, FontStyle.Bold);
 
-        public static Font FontANH_default = new Font("Segoe UI Black", 8, FontStyle.Regular);
+        public static Font Font_default = new Font("Segoe UI Black", 8, FontStyle.Regular);
 
-        public static Font FontANH_MenuMain = new Font("Segoe UI Black", 10, FontStyle.Bold);
-        public static Font FontANH_MenuSub = new Font("Segoe UI Black", 9, FontStyle.Bold);
+        public static Font Font_MenuMain = new Font("Segoe UI Black", 10, FontStyle.Bold);
+        public static Font Font_MenuSub = new Font("Segoe UI Black", 9, FontStyle.Bold);
 
 
-        public static Color MainBG = Color.SeaShell;
+        public static Color Color_MainBG = Color.SeaShell;
 
-        public static Color ColorANH_MenuMainBack = Color.Salmon;
-        public static Color ColorANH_MenuSubBack = Color.LightSalmon;
+        public static Color Color_ButtonMainBack = Color.Salmon;
+        public static Color Color_ButtonSubBack = Color.LightSalmon;
     }
 }
