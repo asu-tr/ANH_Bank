dde53babd0800e88daa89bfd34b236abe363d933
asu-tr <29000874+asu-tr@users.noreply.github.com>
2022-02-21 13:39:39 +0300

    New Colors and Fonts

--- ANHColorsFonts.cs
+++ ANHColorsFonts.cs
@@ -7,6 +7,16 @@ namespace ANH_Bank
         public static Font FontANH_header1 = new Font("Segoe UI Black", 20, FontStyle.Bold);
         public static Font FontANH_header2 = new Font("Segoe UI Black", 15, FontStyle.Bold);
         public static Font FontANH_header3 = new Font("Segoe UI Black", 11, FontStyle.Bold);
+
         public static Font FontANH_default = new Font("Segoe UI Black", 8, FontStyle.Regular);
+
+        public static Font FontANH_MenuMain = new Font("Segoe UI Black", 10, FontStyle.Bold);
+        public static Font FontANH_MenuSub = new Font("Segoe UI Black", 9, FontStyle.Bold);
+
+
+        public static Color MainBG = Color.SeaShell;
+
+        public static Color ColorANH_MenuMainBack = Color.Salmon;
+        public static Color ColorANH_MenuSubBack = Color.LightSalmon;
     }
 }
