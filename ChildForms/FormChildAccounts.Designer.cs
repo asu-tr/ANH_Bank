6aefb22f945fc0400a085685663fb9b59f8f9af1
asu-tr <29000874+asu-tr@users.noreply.github.com>
2022-02-22 21:30:43 +0300

    panel creation for each account

--- ChildForms/FormChildAccounts.Designer.cs
+++ ChildForms/FormChildAccounts.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace ANH_Bank.ChildForms
+﻿using System.Windows.Forms;
+
+namespace ANH_Bank.ChildForms
 {
     partial class FormChildAccounts
     {
@@ -23,6 +25,9 @@
         private void MyInitializeComponent()
         {
             this.BackColor = ANHColorsFonts.Color_MainBG;
+
+            this.labelTotal.Font = ANHColorsFonts.Font_header3;
+            this.labelTotalVal.Font = ANHColorsFonts.Font_header3;
         }
 
         #region Windows Form Designer generated code
@@ -33,19 +38,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelTotal = new System.Windows.Forms.Panel();
+            this.labelTotalVal = new System.Windows.Forms.Label();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.panelTotal.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panelTotal
+            // 
+            this.panelTotal.Controls.Add(this.labelTotalVal);
+            this.panelTotal.Controls.Add(this.labelTotal);
+            this.panelTotal.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTotal.Location = new System.Drawing.Point(0, 0);
+            this.panelTotal.Name = "panelTotal";
+            this.panelTotal.Size = new System.Drawing.Size(408, 75);
+            this.panelTotal.TabIndex = 0;
+            // 
+            // labelTotalVal
+            // 
+            this.labelTotalVal.AutoSize = true;
+            this.labelTotalVal.Location = new System.Drawing.Point(250, 31);
+            this.labelTotalVal.Name = "labelTotalVal";
+            this.labelTotalVal.Size = new System.Drawing.Size(88, 16);
+            this.labelTotalVal.TabIndex = 1;
+            this.labelTotalVal.Text = "labelTotalVal";
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.Location = new System.Drawing.Point(13, 31);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(68, 16);
+            this.labelTotal.TabIndex = 0;
+            this.labelTotal.Text = "labelTotal";
             // 
             // FormChildAccounts
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(408, 606);
+            this.Controls.Add(this.panelTotal);
             this.Name = "FormChildAccounts";
             this.Text = "FormChildAccounts";
+            this.Load += new System.EventHandler(this.FormChildAccounts_Load);
+            this.panelTotal.ResumeLayout(false);
+            this.panelTotal.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelTotal;
+        private System.Windows.Forms.Label labelTotalVal;
+        private System.Windows.Forms.Label labelTotal;
     }
 }
--- ChildForms/FormChildAccounts.cs
+++ ChildForms/FormChildAccounts.cs
@@ -1,15 +1,86 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace ANH_Bank.ChildForms
 {
     public partial class FormChildAccounts : Form
     {
-        public FormChildAccounts()
+        int UserID = -1;
+
+        public FormChildAccounts(int id)
         {
             RefreshForm();
+            UserID = id;
         }
 
+        #region Events
+
+        private void FormChildAccounts_Load(object sender, System.EventArgs e)
+        {
+            foreach (string account in GetAccounts(UserID))
+            {
+                CreateAccountPanel(account);
+            }
+
+            BringTotalToFront();
+        }
+
+        #endregion
+
         #region Methods
+
+        private void BringTotalToFront()
+        {
+            this.Controls.Remove(panelTotal);
+            this.Controls.Add(panelTotal);
+        }
+
+        private void CreateAccountPanel(string account)
+        {
+            Panel panel = new Panel();
+            panel.Name = account; // panel+account.id
+            panel.Tag = account; // account.id
+            panel.Size = new System.Drawing.Size(408, 70);
+            panel.Dock = DockStyle.Top;
+
+            panel.DoubleClick += OpenAccountDetails;
+
+            this.Controls.Add(panel);
+
+            Label labelAcc = new Label();
+            labelAcc.Name = account; // labelAcc+account.id
+            labelAcc.Text = account; // account.accountname
+            labelAcc.Dock = DockStyle.Left;
+            labelAcc.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            labelAcc.Font = ANHColorsFonts.Font_header2;
+
+            Label labelVal = new Label();
+            labelVal.Name = account; // labelVal+account.id
+            labelVal.Text = account; // account.balance
+            labelVal.Dock = DockStyle.Left;
+            labelVal.Padding = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            labelVal.Font = ANHColorsFonts.Font_header2;
+
+            panel.Controls.Add(labelAcc);
+            panel.Controls.Add(labelVal);
+        }
+
+        private List<string> GetAccounts(int id)
+        {
+            List<string> accounts = new List<string>();
+
+            //
+            // code goes here (list type is string for now)
+            // order by asc balance
+            //
+
+            return accounts;
+        }
+
+        private void OpenAccountDetails(object sender, System.EventArgs e)
+        {
+            // code goes here
+        }
 
         private void RefreshForm()
         {
