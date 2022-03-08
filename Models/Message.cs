4b5f161430b37dbb41d70b0ecd46067c63efe514
asu-tr <29000874+asu-tr@users.noreply.github.com>
2022-03-08 14:45:18 +0300

    Messages and MessageTranslations added.

---
+++ Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ANH_Bank.Models
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public virtual List<MessageTranslation> MessageTranslations { get; set; }
+    }
+}
---
+++ Models/MessageTranslation.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ANH_Bank.Models
+{
+    public class MessageTranslation
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Language { get; set; }
+        [Required]
+        public bool IsDefault { get; set; }
+        [Required]
+        public string Translation { get; set; }
+        public virtual Message Message { get; set; }
+    }
+}
